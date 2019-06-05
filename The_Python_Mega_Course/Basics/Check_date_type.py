6a65d6e086e30fd9fa66478d4135efb1803dc5a2
mikaelbeat <ryynanenphm@gmail.com>
2019-06-05 12:07:56 +0300

    Update

--- The_Python_Mega_Course/Basics/Check_date_type.py
+++ The_Python_Mega_Course/Basics/Check_date_type.py
@@ -1,10 +1,11 @@
 
 data1 = 5
 data2 = "Text"
-data3 = "6"
+data3 = 3
 
 print("\n***** Data is int *****\n")
 print(type(data1))
+
 
 print("\n***** Data is str *****\n")
 print(type(data2))
@@ -15,3 +16,12 @@ if isinstance(data3, str):
     print("Data is string")
 elif isinstance(data3, int):
     print("Data is interger")
+    
+       
+print("\n***** Check data type another approach *****\n")
+if type(data3) == int:
+    print("Data is integer")
+elif type(data3) == str:
+    print("Data is string")
+else:
+    print("Data type is other than string or integer")
---
+++ The_Python_Mega_Course/Basics/Slicing.py
@@ -0,0 +1,12 @@
+
+
+letters = 'Python'
+print(letters[-3:-1])
+
+
+
+mylist = ["Marry", "Jack"]
+mylist.append("John")
+print(mylist)
+
+
