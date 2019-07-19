6ad66118d6e88f2df2d246b1240ce7f20d6dd468
mikaelbeat <ryynanenphm@gmail.com>
2019-07-19 23:15:17 +0300

    Update

--- The_Python_Mega_Course/Face_detection/Face_detector.py
+++ The_Python_Mega_Course/Face_detection/Face_detector.py
@@ -2,17 +2,18 @@ import cv2
 
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-image = cv2.imread("photo.jpg")
+image = cv2.imread("news.jpg")
 
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-faces = face_cascade.detectMultiScale(gray_image, scaleFactor = 1.05, minNeighbors = 5)
+faces = face_cascade.detectMultiScale(gray_image, scaleFactor = 1.1, minNeighbors = 5)
 
 print(faces)
 
-# cv2.imshow("Gray", gray_image)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+for x, y, w, h in faces:
+    image = cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 3)
 
 
-# 11:55
+cv2.imshow("Gray", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
